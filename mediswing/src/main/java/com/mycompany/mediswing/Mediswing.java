dccb01b92a2399e7a063d53ced06b21d0d15ab2a
Caio Louback <Caio Louback@PC-CTGA>
2025-12-26 17:57:46 -0300

    123321

---
+++ mediswing/src/main/java/com/mycompany/mediswing/Mediswing.java
@@ -0,0 +1,12 @@
+
+
+package com.mycompany.mediswing;
+
+
+public class Mediswing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("capetaasi diasdnjisainda");
+    }
+}
